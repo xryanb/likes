1f1b18673f6c03174c6461c1484be5599f11acfd
ryan bautista <96922150+xryanb@users.noreply.github.com>
2022-03-28 22:53:19 -0700

    Add files via upload

---
+++ part2/script.js
@@ -0,0 +1,31 @@
+
+/*neil likes*/
+var neilM=9;
+var addOne= document.querySelector('.neil');
+
+function addUp(element) {
+    neilM++;
+    addOne.innerText= neilM + " like(s)";
+}
+
+/* nichole likes */
+var nicholeK=12;
+var anotherOne=document.querySelector('.nichole');
+
+function plusOne(element) {
+    nicholeK++;
+    anotherOne.innerText= nicholeK + " like(s)";
+}
+
+
+/*jim likes */
+var jimR=9;
+var oneUp=document.querySelector('.jim');
+
+function hereOne(element){
+    jimR++;
+    oneUp.innerText= jimR + " like(s)";
+}
+
+
+
